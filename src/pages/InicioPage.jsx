021860e30841b66d98e898d1fe035edb81d6116e
Frauli117 <mcfrauliarce@gmail.com>
2026-04-08 09:23:18 -0600

    Implementaccion de Mi app react para desplegar en railway

---
+++ src/pages/InicioPage.jsx
@@ -0,0 +1,13 @@
+function InicioPage() {
+  return (
+    <div>
+      <h1>Sistema de gestión</h1>
+      <p>Bienvenido a la aplicación React conectada con la API en Express.</p>
+      <p>
+        Usa el menú para ingresar al módulo de usuarios, productos, clientes o proveedores.
+      </p>
+    </div>
+  );
+}
+
+export default InicioPage;
